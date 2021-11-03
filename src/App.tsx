49528fd241643c47896ddf779747f2b672febfd7
조민혁 <chohyuk0725@gmail.com>
2021-11-03 15:02:42 +0900

    immer 적용전

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,97 @@
-import React from "react";
+import React, { useRef, useState, useCallback } from "react";
+import produce from "immer";
 import logo from "./logo.svg";
 import "./App.css";
+type DataArrayObject = {
+  id: number;
+  name: string;
+  username: string;
+};
+type DataArray = DataArrayObject[];
+type Data = {
+  array: DataArray;
+  uselessValue: null | string;
+};
 
 function App() {
+  const nextId = useRef(1);
+
+  const [form, setForm] = useState({ name: "", userName: "" });
+  const dataInit: Data = {
+    array: [],
+    uselessValue: null,
+  };
+  const [data, setData] = useState(dataInit);
+
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setForm({
+        ...form,
+        [name]: [value],
+      });
+    },
+    [form]
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info: DataArrayObject = {
+        id: nextId.current,
+        name: form.name,
+        username: form.userName,
+      };
+      setData({
+        ...data,
+        array: data.array.concat(info),
+      });
+      setForm({
+        name: "",
+        userName: "",
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.userName]
+  );
+  const onRemove = useCallback(
+    (id) => {
+      setData({
+        ...data,
+        array: data.array.filter((info) => info.id !== id),
+      });
+    },
+    [data]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="userName"
+          placeholder="아이디"
+          value={form.userName}
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="이름"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username}({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
